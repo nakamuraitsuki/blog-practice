9db92046ad750ba079b884e8d92d84f113f6c7b6
nakamura <itsuki1911n@gmail.com>
2024-01-22 20:00:54 +0900

    編集、削除機能の追加。

--- main.go
+++ main.go
@@ -16,14 +16,25 @@ import (
 const (
 	templatePath = "./template"
 	layoutPath   = templatePath + "/layout.html"
-	dbPath       = "./db.sqlite3"
 	createPath   = templatePath + "/create.html"
+	editPath     = templatePath + "/edit.html"
+
+	dbPath       = "./db.sqlite3"
+	publicPath   = "./public"
+
+
 	// ブログポストテーブルにデータを挿入するSQL文
 	insertPostQuery = `INSERT INTO posts (title, body, author, created_at) VALUES (?, ?, ?, ?)`
 	//ブログポストテーブルからデータを取得(idで)
 	selectPostByIdQuery = `SELECT * FROM posts WHERE id = ?`
 	// ブログポストテーブルから全てのデータを取得するSQL文
 	selectAllPostsQuery = `SELECT * FROM posts`
+	// ブログポストテーブルのデータを削除するSQL文
+	deletePostQuery = `DELETE FROM posts WHERE id = ?`
+	// ブログポストテーブルのデータを更新するSQL文
+    updatePostQuery = `UPDATE posts SET title = ?, body = ?, author = ?, created_at = ? WHERE id = ?`
+
+
 )
 
 type Post struct {
@@ -35,7 +46,7 @@ type Post struct {
 }
 
 var (
-	indexTemplate        = template.Must(template.ParseFiles(layoutPath, templatePath+"/index.html"))
+	indexTemplate = template.Must(template.New("layout.html").Funcs(funcDate).ParseFiles(layoutPath, templatePath+"/index.html"))
 	postTemplate  = template.Must(template.ParseFiles(layoutPath,templatePath+"/post.html"))
 	db                   *sqlx.DB
 	createPostTableQuery = `CREATE TABLE IF NOT EXISTS posts (
@@ -45,7 +56,14 @@ var (
         author TEXT,
         created_at INTEGER
     )`
+
 	createTemplate = template.Must(template.ParseFiles(layoutPath, createPath))
+	editTemplate   = template.Must(template.ParseFiles(layoutPath, editPath))
+	funcDate = template.FuncMap{
+        "date": func(t int64) string {
+            return time.Unix(t, 0).Format("2006-01-02 15:04:05")
+        },
+    }
 	
 )
 
@@ -60,6 +78,14 @@ func main() {
 	http.HandleFunc("/", IndexHandler)
 	http.HandleFunc("/post/",postHandler)
 	http.HandleFunc("/post/new", createPostHandler)
+	http.HandleFunc("/post/delete/", deletePostHandler)
+	http.HandleFunc("/post/edit/", editPostHandler)
+
+	//cssを配信
+	// cssファイルを配信
+    http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir(publicPath+"/css"))))
+
+	
 	fmt.Println("Server is listening on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -114,6 +140,7 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
     // テンプレートを表示
     postTemplate.ExecuteTemplate(w, "layout.html", map[string]interface{}{
         "Title": post.Title,
+		"ID":post.ID,
         "PageTitle": post.Title,
         "Body":      post.Body,
 		"CreatedAt": time.Unix(post.CreatedAt,0).Format("2006-01-02 15:04:05"),
@@ -195,4 +222,96 @@ func getAllPosts() ([]Post, error) {
         return posts, err
     }
     return posts, nil
+}
+
+func editPostHandler(w http.ResponseWriter, r *http.Request) {
+    // URLのPathからIDを取得
+    id := r.URL.Path[len("/post/edit/"):]
+    //idをint型に変換
+    idInt, err := strconv.Atoi(id)
+    if err != nil {
+        log.Print(err)
+        // InternalServerErrorを返す
+        return
+    }
+    if r.Method == "GET" {
+        // GETリクエストの場合はテンプレートを表示
+        post, err := getPostById(idInt)
+        if err != nil {
+            log.Print(err)
+            // InternalServerErrorを返す
+            return
+        }
+        editTemplate.ExecuteTemplate(w, "layout.html", map[string]interface{}{
+            "PageTitle": "ブログポスト編集",
+            "ID":        post.ID,
+            "Title":     post.Title,
+            "Body":      post.Body,
+            "Author":    post.Author,
+        })
+    } else if r.Method == "POST" {
+        // POSTリクエストの場合はブログポストを更新
+        title := r.FormValue("title")
+        body := r.FormValue("body")
+        author := r.FormValue("author")
+        createdAt := time.Now().Unix()
+        // フォームに空の項目がある場合はエラーを返す
+        if title == "" || body == "" || author == "" {
+            log.Print("フォームに空の項目があります")
+            editTemplate.ExecuteTemplate(w, "layout.html", map[string]interface{}{
+                "Message": "フォームに空の項目があります",
+            })
+            return
+        }
+        err := updatePostByID(idInt, title, body, author, createdAt)
+        if err != nil {
+            log.Print(err)
+            return
+        }
+        http.Redirect(w, r, "/post/"+strconv.Itoa(idInt), http.StatusFound)
+    }
+}
+
+// ブログポストを削除
+func deletePostByID(id int) error {
+    // ブログポストを削除
+    _, err := db.Exec(deletePostQuery, id)
+    if err != nil {
+        log.Print(err)
+        // InternalServerErrorを返す
+        return err
+    }
+    return nil
+}
+
+func deletePostHandler(w http.ResponseWriter, r *http.Request) {
+    // URLのPathからIDを取得
+    id := r.URL.Path[len("/post/delete/"):]
+    //idをint型に変換
+    idInt, err := strconv.Atoi(id)
+    if err != nil {
+        log.Print(err)
+        // InternalServerErrorを返す
+        return
+    }
+    // ブログポストを削除
+    err = deletePostByID(idInt)
+    if err != nil {
+        log.Print(err)
+        // InternalServerErrorを返す
+        return
+    }
+    http.Redirect(w, r, "/", http.StatusFound)
+}
+
+// ブログポストを更新
+func updatePostByID(id int, title string, body string, author string, createdAt int64) error {
+    // ブログポストを更新
+    _, err := db.Exec(updatePostQuery, title, body, author, createdAt, id)
+    if err != nil {
+        log.Print(err)
+        // InternalServerErrorを返す
+        return err
+    }
+    return nil
 }
